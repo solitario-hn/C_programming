fd0025184e27789b06c4a1a38892b7fea3937c5f
solitario <elsolitariolafe@gmail.com>
2026-01-18 17:52:03 +0000

    bgugi

---
+++ Basic-C/calc.c
@@ -0,0 +1,47 @@
+//Write a C Program to implement Simple Calculator using the concept of function
+
+//Write a program to find product,sum,average, max and min from a list of n numbers
+
+#include <stdio.h>
+
+void main(){
+    char operation[1];
+    printf("Enter the operation you wish to perform\n1.'S' Subtract\n2.'A' Addition\n3.'P' Product\n4.'D' Divide:\n ");
+    scanf("%c",operation);
+}
+
+
+
+
+
+
+
+int operation(char operation,int numbers[10]){
+    switch(operation){
+        case 'A':
+
+    }
+}
+
+
+
+
+
+
+
+int addition(int number[]){
+    
+}
+
+int product(int number[]){
+
+}
+
+int divide(int number[]){
+
+}
+
+int subtract(int number[]){
+
+}
+
