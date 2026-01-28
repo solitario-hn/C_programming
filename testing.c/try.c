b78d472b979ef53625981fb5dc7cbaddbc29deee
solitario <elsolitariolafe@gmail.com>
2026-01-28 18:47:03 +0000

    idk man

---
+++ testing.c/try.c
@@ -0,0 +1,8 @@
+//pointer variable act- charm*something.
+//typedef use as a dict not exactly
+//memory allocation and passing on memory value
+//dereferencing and ref rence during printf
+//compile input 
+//format specifiers do the work.
+//creating your own strcmp , strlen
+
