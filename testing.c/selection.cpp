6dc3f326ec52c79da8f77f6d75cfc161f4324c35
solitario <elsolitariolafe@gmail.com>
2026-03-10 17:23:09 +0000

    sorting

--- testing.c/selection.cpp
+++ testing.c/selection.cpp
@@ -24,23 +24,23 @@
 
 //INSERTION SORT
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
 
-int main(){
-    int arr[5]={7,8,6,5,4};
-    for(int i=1;i<=5;i++){
-        int key=arr[i];
-        int j=i - 1;
-        while (j >= 0 &&arr[j]>key){//checking the if conditions in the while declaration.
-            arr[j+1]=arr[j];
-            arr[j]=key;
-            j--;  
-        }        
-    }
-    for(int i=1;i<=5;i++){
-        cout<<arr[i]<<',';
-    }
-    cout<<"\n";
-}
+// int main(){
+//     int arr[5]={7,8,6,5,4};
+//     for(int i=1;i<=5;i++){
+//         int key=arr[i];
+//         int j=i - 1;
+//         while (j >= 0 &&arr[j]>key){//checking the if conditions in the while declaration.
+//             arr[j+1]=arr[j];
+//             arr[j]=key;
+//             j--;  
+//         }        
+//     }
+//     for(int i=1;i<=5;i++){
+//         cout<<arr[i]<<',';
+//     }
+//     cout<<"\n";
+// }
