3d6c9a2655f4ca58cf9646b1e6e245313373d6a6
solitario <elsolitariolafe@gmail.com>
2026-03-20 19:49:57 +0000

    idja

--- Basic-C/test.c
+++ Basic-C/test.c
@@ -1,35 +1,39 @@
-#include <stdio.h>
-#include <string.h>
-//index of smallest and largest array
+// #include <stdio.h>
+// #include <string.h>
+// //index of smallest and largest array
 
-/*
-int main() {
-    int count=0;
-    for(int i=0;i<9;i++)
-    {
-        count+=1;
-    }
-    printf("%d",count);
-  return 0;
-}
-*/
+// /*
+// int main() {
+//     int count=0;
+//     for(int i=0;i<9;i++)
+//     {
+//         count+=1;
+//     }
+//     printf("%d",count);
+//   return 0;
+// }
+// */
 
-int main(){
-    int array[10]={100,200,300,400,500,600,700,800,900,1000};
-    int max=0;
-    int min=0;
-    for(int i=1;i<10;i++){
-        if(array[min]>array[i]){
-            min=i;
-        }
-        if(array[max]<array[i]){
-            max=i;
-        }
+// int main(){
+//     int array[10]={100,200,300,400,500,600,700,800,900,1000};
+//     int max=0;
+//     int min=0;
+//     for(int i=1;i<10;i++){
+//         if(array[min]>array[i]){
+//             min=i;
+//         }
+//         if(array[max]<array[i]){
+//             max=i;
+//         }
     
-    }
+//     }
 
-printf("this is the max value %d,this is the min value %d",max,min);
-
+// printf("this is the max value %d,this is the min value %d",max,min);
     
-}
+// }
 
+// Write a C program to input the age of a person in years and display the equivalent age in months, days, and
+// hours.
+
+//1year=12 months
+//1 month=30 days
